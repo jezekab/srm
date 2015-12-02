d89c9ae5e5a267f7927e32205ccd6cd9f8ed2000
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-12-02 12:07:59 +1000

    Adjusted Contact with sub classes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,3 +5,18 @@ class Contact
   field :email, type: String
   field :number, type: String
 end
+
+  class Student < Contact
+    field :student_number, type: String, default: 'n0000000'
+    #field :faculty, type Symbol
+    #field :year, type Symbol
+  end
+
+    class Executive < Student
+      field :position, type: Symbol
+    end
+
+  class Professional < Contact
+    field :workplace, type: String
+    field :relation, type: String
+  end
