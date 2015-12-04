502e6057f0077d93b68e55fe2b835c83df9c028b
Byron Mejia <byron.mejia@connect.qut.edu.au>
2015-12-04 17:20:02 +1000

    Removed all startups scaffolding: Because I forgot to fork before continuing.

--- app/models/startup.rb
+++
@@ -1,8 +0,0 @@
-class Startup
-  include Mongoid::Document
-  field :name, type: String
-  field :type, type: Symbol
-  field :founded_date, type: Date
-  field :stage, type: Symbol
-  embedded_in :contact
-end
--- app/views/startups/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @startup, :id, :name, :type, :founded_date, :stage, :contact_id, :created_at, :updated_at
